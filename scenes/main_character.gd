de99525306846294fce1c5503840a43a7237ec4b
marianoluiz <kmarianu@gmail.com>
2024-10-27 18:27:40 +0800

    added coyote

--- scenes/main_character.gd
+++ scenes/main_character.gd
@@ -4,6 +4,11 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -800.0
 @onready var sprite_2d = $Sprite2D # drag sprite 2d then ctrl
 
+# coyote and jump buffering
+const COYOTE_TIME = 0.2  # Allow 0.2 seconds of coyote time
+var coyote_timer = 0.0
+var has_jumped = false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -20,10 +25,22 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		sprite_2d.animation = "jumping"
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
+	# Handle jump.
+	
+	# Coyote time logic
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME  # Reset the timer when on the ground
+		has_jumped = false  # Reset jump state when on the ground
+	else:
+		coyote_timer -= delta  # Count down if in the air
+
+	# Jump logic
+	if Input.is_action_just_pressed("jump"):
+		if not has_jumped and (is_on_floor() or coyote_timer > 0):
+			velocity.y = JUMP_VELOCITY
+			has_jumped = true  # Set to true to prevent another jump
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
